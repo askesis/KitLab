411a8e3e3c03f8b56be8d8cda1e5802f9e9b541b
askesis <oleg.bareev@yandex.ru>
2017-07-30 18:32:02 +0300

    restructure

--- src/TransactionFilters.jsx
+++ src/TransactionFilters.jsx
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import TransactionTables from "./TransactionTables.jsx";
 import { Button, ButtonToolbar, Grid, Row, Col } from 'react-bootstrap';
 
-var TransactionFiltered = React.createClass({
-    render: function(){
-        return(
-            <ButtonToolbar>
-                <Button>one</Button>
-                <Button>two</Button>
-            </ButtonToolbar>
-            
-        )
-    }
+const TransactionFiltered = React.createClass({
+  render: function(){
+    return(
+    <ButtonToolbar>
+    <Button>one</Button>
+        <Button>two</Button>
+      </ButtonToolbar>
+      
+    )
+  }
 })
 
 export default TransactionFiltered;
