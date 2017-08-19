112441e4c646af43621f1eddc889205cdae5ff3c
askesis <oleg.bareev@yandex.ru>
2017-08-19 16:33:38 +0300

    I cant think of a name for the commit

--- src/menu.jsx
+++ src/menu.jsx
@@ -4,38 +4,14 @@ import { Panel, Button } from 'react-bootstrap';
 
 
 class Menu extends Component{
-  constructor(props){
-    super(props);
-
-    this.state = {
-      activeBtn:{},
-    }
-  }
-
-  handleBtnMenuActivity = btn =>{
-  //   this.setState({
-  //     activeBtn: {...this.state.activeBtn, [btn]:btn }
-  //   })
-   }
 
   render(){
-{/*
-    const btnMenuAdd = this.state.activeBtn['add'] ? 'primary' : 'default';bsStyle={btnMenuAdd}
-    const btnMenuTable = this.state.activeBtn['table'] ? 'primary' : 'default';
     return(
       <Panel>
-        <NavLink to='/form_add'><Button activeClassName="selected" onClick={ () => this.handleBtnMenuActivity('add') }  > add transaction </Button></NavLink>
-        <NavLink to='/'><Button activeClassName="selected" onClick={ () => this.handleBtnMenuActivity('table') } > table </Button></NavLink>
-      </Panel>
-    )*/}
-
-    return(
-      <Panel>
-       <NavLink to='/form_add' activeClassName="selected"> add transaction </NavLink>
-        <NavLink to='/' activeClassName="selected"> table </NavLink>
+        <NavLink to='/form_add'><Button > add transaction </Button></NavLink>
+        <NavLink to='/'><Button > table </Button></NavLink>
       </Panel>
     )
-
   }
 }
 
